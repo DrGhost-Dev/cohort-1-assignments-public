1906c446c6d8cf30a3330ba0bd0eade8720c5600
DrGhost-Dev <jihwan2843@gmail.com>
2025-09-05 10:10:21 +0900

    feat: geth and geth-init

--- 1b/geth-init/prefund.js
+++ 1b/geth-init/prefund.js
@@ -1,7 +1,10 @@
 const from = eth.accounts[0];
-const contractDeployer = "0x3dC104B90b963a1d4D004517B2D6d530952Db75c";
+const contractDeployer = "0xEA56b22c446A5fbEd33c231feCD42A1d78641119";
 eth.sendTransaction({
   from: from,
   to: contractDeployer,
   value: web3.toWei(100, "ether"),
 });
+
+console.log("Successfully pre-funded 100 ETH to " + contractDeployer);
+
